a9ac09fca161db8573bbdbe33bc7f044ae51ead6
ABU-BAW <abubawie.24@gmail.com>
2026-04-04 12:02:12 +0000

    not found page added

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+
+
+
+function NotFound() {
+    return ( 
+        <div className="text-base tracking-wide m-auto">
+            <p >OOPs...Page not Found!!!</p>
+        </div>
+     );
+}
+
+export default NotFound;
